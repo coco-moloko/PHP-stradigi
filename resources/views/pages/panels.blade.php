3f14a103d30bc9fe5f3d343d88a60f7447e07802
Coco Moloko <courtneyb.brecht@gmail.com>
2017-04-11 14:38:39 +0000

    added panels fix

--- resources/views/pages/panels.blade.php
+++ resources/views/pages/panels.blade.php
@@ -10,8 +10,8 @@
 <body>
     <div class="bodyContent">
             <div class="logoCircle"><img src="{{ URL::asset('img/img-kraken.png') }}"></img></div>
-                <h1 class="myTitle">This is test content to be printed. This is test content to be printed.</h1>
-                <p class="myText">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Fusce nec eros dictum, feugiat nulla sit amet, fermentum nibh. Aliquam id semper mauris. Nunc sit amet lacus id nibh molestie porta. Aliquam non magna non erat facilisis imperdiet. Suspendisse consequat lobortis lacinia. Nam laoreet aliquam enim vitae cursus. Nulla non  mauris augue, in sollicitudin dui lacinia sed Nulla d, egestas et neque.</p>
+                <h1 class="myTitle"></h1>
+                <p class="myText"></p>
     </div>
     
     
