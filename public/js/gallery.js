4a024c53068fe61dd9cd2a81eadf87a307db36b1
Coco Moloko <courtneyb.brecht@gmail.com>
2017-04-12 20:50:15 +0000

    lightbox fixed

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -11,28 +11,35 @@ let xhr = $.get("https://api.giphy.com/v1/gifs/search?q=ryan+gosling&api_key=dc6
    
     var imgSrc =$(e).attr('src');
     $('html').addClass('no-scroll');
-    $('.open-lightbox').html('<img src="' + imgSrc + '">').addClass('lightbox-opened');
+    $(".open-lightbox").children().hide();
+    $(e).show();
+    $('.open-lightbox').addClass('lightbox-opened');
         }
   //});
   
   $('.open-lightbox').on('click', '.lightbox-opened', function() {
+      $('.open-lightbox').removeClass('lightbox-opened');
+      $(".open-lightbox").children().show();
     $('html').removeClass('no-scroll');
-    $('.open-lightbox').removeClass('lightbox-opened');
   });
 
  (function($) {
   //var image = $(this).attr('href');
   // Open Lightbox
- 
+ $('html').click(function(e){
+   if(!$(e.target).hasClass('responsive-image') )
+   {
+      $('.open-lightbox').removeClass('lightbox-opened');
+      $(".open-lightbox").children().show();
+    $('html').removeClass('no-scroll');              
+   }
+ });
   
   // Close Lightbox
-    $('.open-lightbox').on('click', '.open-lightbox', function() {
+    $('.open-lightbox').on('click', '.lightbox-opened', function() {
+     $('.open-lightbox').removeClass('lightbox-opened');
+      $(".open-lightbox").children().show();
     $('html').removeClass('no-scroll');
-    $('.open-lightbox').removeClass('lightbox-opened');
   });
   
 })(jQuery);
-
-//Can you please take to cross button html?
-//its made all in CSS
-//ok
